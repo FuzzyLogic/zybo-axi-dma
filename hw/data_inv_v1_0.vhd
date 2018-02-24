47b567aa0a3a76028c4149633066b7ebfe8bdfa4
Christian P. Feist <cfeist89@googlemail.com>
2018-02-24 09:57:50 +0100

    Added the inversion core HDL

---
+++ hw/data_inv_v1_0.vhd
@@ -0,0 +1,60 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity data_inv_v1_0 is
+	generic (
+		-- Users to add parameters here
+
+		-- User parameters ends
+		-- Do not modify the parameters beyond this line
+
+
+		-- Parameters of Axi Slave Bus Interface S00_AXIS
+		C_S00_AXIS_TDATA_WIDTH	: integer	:= 32;
+
+		-- Parameters of Axi Master Bus Interface M00_AXIS
+		C_M00_AXIS_TDATA_WIDTH	: integer	:= 32;
+		C_M00_AXIS_START_COUNT	: integer	:= 32
+	);
+	port (
+		-- Users to add ports here
+
+		-- User ports ends
+		-- Do not modify the ports beyond this line
+
+
+		-- Ports of Axi Slave Bus Interface S00_AXIS
+		s00_axis_aclk	: in std_logic;
+		s00_axis_aresetn	: in std_logic;
+		s00_axis_tready	: out std_logic;
+		s00_axis_tdata	: in std_logic_vector(C_S00_AXIS_TDATA_WIDTH-1 downto 0);
+		s00_axis_tstrb	: in std_logic_vector((C_S00_AXIS_TDATA_WIDTH/8)-1 downto 0);
+		s00_axis_tlast	: in std_logic;
+		s00_axis_tvalid	: in std_logic;
+
+		-- Ports of Axi Master Bus Interface M00_AXIS
+		m00_axis_aclk	: in std_logic;
+		m00_axis_aresetn	: in std_logic;
+		m00_axis_tvalid	: out std_logic;
+		m00_axis_tdata	: out std_logic_vector(C_M00_AXIS_TDATA_WIDTH-1 downto 0);
+		m00_axis_tstrb	: out std_logic_vector((C_M00_AXIS_TDATA_WIDTH/8)-1 downto 0);
+		m00_axis_tlast	: out std_logic;
+		m00_axis_tready	: in std_logic
+	);
+end data_inv_v1_0;
+
+architecture arch_imp of data_inv_v1_0 is
+begin
+	-- Add user logic here
+	
+	-- Invert slave interface tdata and connect directly to master interface
+	m00_axis_tvalid	<= s00_axis_tvalid;
+    m00_axis_tdata  <= not s00_axis_tdata;
+    m00_axis_tstrb  <= s00_axis_tstrb;
+    m00_axis_tlast  <= s00_axis_tlast;
+    s00_axis_tready <= m00_axis_tready;
+
+	-- User logic ends
+
+end arch_imp;
